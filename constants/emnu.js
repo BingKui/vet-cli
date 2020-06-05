7e4b24ee71bb9df4925e80dfdc2bc9a846a26dc2
BingKui <178974662@qq.com>
2020-06-05 16:58:45 +0800

    版本升级，更新功能

---
+++ constants/emnu.js
@@ -0,0 +1,60 @@
+const ENMU = {
+    standard: {
+        branch: '#master',
+        name: '默认模板(Default)',
+        value: 'standard',
+        short: 'Default',
+        homepage: '',
+        desc: '默认模板，包含基础的配置，提供最大的自由度可定制。',
+    },
+    iview: {
+        branch: '#iview',
+        name: 'iview模板',
+        value: 'iview',
+        short: 'View UI',
+        homepage: 'https://www.iviewui.com/docs/introduce',
+        desc: 'View UI，即原先的 iView，是一套基于 Vue.js 的开源 UI 组件库，主要服务于 PC 界面的中后台产品。',
+    },
+    element: {
+        branch: '#element',
+        name: 'element模板',
+        value: 'element',
+        short: 'Element',
+        homepage: 'https://element.eleme.cn/#/zh-CN/component/installation',
+        desc: 'Element，一套为开发者、设计师和产品经理准备的基于 Vue 2.0 的桌面端组件库。',
+    },
+    antd: {
+        branch: '#antd',
+        name: 'Ant Design Vue模板',
+        value: 'antd',
+        short: 'Ant Design Vue',
+        homepage: 'https://www.antdv.com/docs/vue/introduce-cn/',
+        desc: 'Ant Design 的 Vue 实现，开发和服务于企业级后台产品。',
+    },
+    at: {
+        branch: '#at',
+        name: 'AT UIKIT模板',
+        value: 'at',
+        short: 'AT UIKIT',
+        homepage: 'https://at-ui.github.io/at-ui/#/zh/docs/introduction',
+        desc: 'AT-UI 是一款基于 Vue.js 2.0 的前端 UI 组件库，主要用于快速开发 PC 网站中后台产品。',
+    },
+    material: {
+        branch: '#material',
+        name: 'Vue Material模板',
+        value: 'material',
+        short: 'Vue Material',
+        homepage: 'http://vuematerial.materializecss.cn/#/',
+        desc: 'Vue Material 是一个轻量级的框架， 建立在谷歌的 Material Design 基础上。 设计强大的和美观的web应用并适用于不同的屏幕。',
+    },
+    muse: {
+        branch: '#muse',
+        name: 'Muse-UI模板',
+        value: 'muse',
+        short: 'Muse-UI',
+        homepage: 'https://muse-ui.org/#/zh-CN/installation',
+        desc: '基于 Vue 2.0 优雅的 Material Design UI 组件库。',
+    },
+};
+
+module.exports = ENMU;
